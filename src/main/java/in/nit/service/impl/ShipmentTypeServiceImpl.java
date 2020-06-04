163ccdaba9d06da72a0ffd41ffd4122039b0f1c3
abhinandan <abhinandanc3388@gmail.com>
2020-06-04 16:24:47 +0530

    working app day1

--- src/main/java/in/nit/service/impl/ShipmentTypeServiceImpl.java
+++ src/main/java/in/nit/service/impl/ShipmentTypeServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import in.nit.model.ShipmentType;
 import in.nit.repo.IShipmentTypeRepository;
 import in.nit.service.IShipmentTypeService;
 
+@Service
 public class ShipmentTypeServiceImpl implements IShipmentTypeService {
     @Autowired
 	IShipmentTypeRepository repo;
